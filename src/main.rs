793ac8692b7dd7ac85b24480100e51e118fa7490
kim <taterkimmies@gmail.com>
2024-04-03 23:02:48 -0400

    do I need map? probably not

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
 use std::fs;
-
+use std::io;
+use std::path::PathBuf;
 
 //lets try using shit like bin search or bubble sorting
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 
-//TODO: 
-// add wildcards like * so I can search for *ss and find ./piss and ./ass
+
 struct Args {
     // Look for query in file or folder name
     #[arg(short = 'q', long)]
@@ -36,14 +36,10 @@ fn main() {
     let mut target_found = false; 
     
     if !args.recursive {
-        let dl = get_dir_list(&args.dir);
-        for d in dl {
-            println!("{}", &d);
-            //if find_query_in_vec(&args.query, &d) {
-            //    println!("Found query {}: {}", &args.query, &d);
-            //    //output_result(&vec![d]);
-            //    //output_result(&vec![d]);
-            //}
+        let dl = get_dir_list(&args.dir).unwrap();
+        for item in dl {
+            println!("Checking {} against {}", &args.query, pathbuf_filename(&item));
+
         }
         //output_result(&dl);
         //println!("{}", find_query_in_vec(&args.query, &dl));
@@ -52,30 +48,43 @@ fn main() {
 
 }
 
-fn find_query_in_vec(query: &str, target: &str) -> bool {
-    let target_file_name = {
-        let path: &str = &target;
-        path.split('/').last().unwrap().to_string()
-    };
-    target_file_name.contains(query)
+fn find_query_in_vec(query: &str, target_vec: &Vec<String>) -> bool {
+    // only supply vector of files, no dirs
+    for item in target_vec {
+        let item_file_name = {
+            let path: &str = &item;
+            path.split('/').last().unwrap().to_string()
+        };
+        println!("Checking {} against {}", &query, &item_file_name);
+        if item_file_name.contains(query) {
+            return true;
+        }
+    }
+    false
 }
 
-fn get_dir_list(dir: &str) -> Vec<String> {
-    let paths = fs::read_dir(dir).unwrap();
-    let mut vec: Vec<String> = Vec::new();
-    for path in paths {
-        vec.push(path.unwrap().path().display().to_string());
-    }
-    vec.sort();
-    // without sort
-    // .\.git
-    // .\.gitignore
-    // .\Cargo.lock
-    // .\Cargo.toml
-    // .\src
-    // .\target
-    vec
+fn get_dir_list(dir: &str) -> Result<Vec<PathBuf>, io::Error> {
+    let paths = fs::read_dir(dir)?;
+    let mut vec_files: Vec<String> = Vec::new();
+    let mut vec_dirs: Vec<String> = Vec::new();
+    
+    let mut entries = fs::read_dir(".")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
 
+    entries.sort();
+
+    Ok(entries)
+
+}
+
+fn pathbuf_filename(pathbuf: &PathBuf) -> String {
+    pathbuf
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string()
 }
 
 
